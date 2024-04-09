20c5289024eeee209aca89e04fc6121113a3ebdc
rohanzore30 <164886795+rohanzore30@users.noreply.github.com>
2024-04-09 14:01:01 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
 
 # Stop the running container (if any)
-containerid='docker ps| awk -F "" '{print $1}''
-docker rm -f containerid
+containerid='docker ps| awk -F "" '{Print $1}''
+docker rm -f $containerid
